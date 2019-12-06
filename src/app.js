e0293a5ee035dc5bc3a20be4ef620a8c08a87d42
jpegzilla <eris@jpegzilla.com>
2019-12-06 12:13:01 -0500

    finished api

--- src/app.js
+++ src/app.js
@@ -1,38 +1,47 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const bookmarksRouter = require('./bookmarks/bookmarks-router')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const bookmarksRouter = require("./bookmarks/bookmarks-router");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, bookmarks!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, bookmarks!");
+});
 
 //app.use AUTHENTICATION FUNCTION
 
-app.use(bookmarksRouter)
+const validateBearerToken = (req, res, next) => {
+  const token = process.env.API_TOKEN;
+  const auth = req.get("Authorization");
+
+  if (!auth || auth.split(" ")[1] !== token)
+    return res.status(401).json({ error: "Unauthorized request" });
+  else next();
+};
+
+app.use(validateBearerToken);
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
-  if(process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error'} }
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
-    console.error(error)
-    response = { message: error.message, error }
+    console.error(error);
+    response = { message: error.message, error };
   }
-  res.status(500).json(response)
-})
+  res.status(500).json(response);
+});
 
-module.exports = app
+app.use(bookmarksRouter);
+
+module.exports = app;
--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,44 +1,38 @@
-const express = require('express');
-const logger = require('../logger');
-const uuid = require('uuid/v4');
+const express = require("express");
+const logger = require("../logger");
+const uuid = require("uuid/v4");
 
 const bookmarksRouter = express.Router();
 const bodyParser = express.json();
 
-const bookmarks = [{
-  id: 1,
-  name: 'Test',
-  description: 'This is a bookmark'
-}];
+const bookmarks = [
+  {
+    id: 1,
+    name: "Test",
+    description: "This is a bookmark"
+  }
+];
 
 const example = {
-  name: 'Test2',
-  description: 'Also a test'
+  name: "Test2",
+  description: "Also a test"
 };
 
 bookmarksRouter
-  .route('/bookmarks')
+  .route("/bookmarks")
   .get((req, res) => {
-    res
-      .json(bookmarks);
+    res.json(bookmarks);
   })
   .post(bodyParser, (req, res) => {
     const { name, description } = req.body;
 
-    console.log(req.body)
-    console.log(name)
-    console.log(description)
-    if(!name) {
-      logger.error('Name is required');
-      return res
-        .status(400)
-        .send('Invalid name');
+    if (!name) {
+      logger.error("Name is required");
+      return res.status(400).send("Invalid name");
     }
-    if(description.length <= 0) {
-      logger.error('Description is required');
-      return res
-        .status(400)
-        .send('Invalid description');
+    if (description.length <= 0) {
+      logger.error("Description is required");
+      return res.status(400).send("Invalid description");
     }
 
     const id = uuid();
@@ -54,7 +48,23 @@ bookmarksRouter
     res
       .status(201)
       .location(`http://localhost:8000/bookmarks/${id}`)
-      .json({id});
+      .json({ id });
   });
+
+bookmarksRouter.route("/bookmarks/:id").get((req, res) => {
+  const { id } = req.params;
+
+  return bookmarks[id]
+    ? res.json(bookmarks[id])
+    : res.status(404).send("invalid id");
+});
+
+bookmarksRouter.route("/bookmarks/:id").delete((req, res) => {
+  const { id } = req.params;
+
+  bookmarks.splice(bookmarks.findIndex(b => b.id == id), 1);
+
+  res.send(204).end();
+});
 
 module.exports = bookmarksRouter;
